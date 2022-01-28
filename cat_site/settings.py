e4740ce0bc183ecc7936c144b512bb9e9260f729
Liqkerin <vida.filip@gmail.com>
2022-01-28 15:04:12 +0100

    Set up the admin area, started using a postgres database and created a model for the cat table

--- cat_site/settings.py
+++ cat_site/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'cat_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'catDatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
--- catapp/models.py
+++ catapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    internalID = models.IntegerField('Cat serial number from the csv')
+    Name = models.CharField('Name of the cat', max_length=100)
+    Breed = models.CharField('Breed of the cat', max_length=100)
+    Birth = models.DateField('Birthdate of the cat')
+    Gender = models.CharField('Gender of the cat', max_length=100)
+    Fur = models.CharField('Fur code of the cat', max_length=100)
+    Number = models.CharField('Identification number of the cat', max_length=200)
+    Title = models.CharField('Titles of the cat', max_length=50)
+    Father = models.CharField('Identification of the Father of the cat', max_length=200)
+    Mother = models.CharField('Identification of the Mother of the cat', max_length=200)
+    From = models.CharField('Site from which the cat is from', max_length=20)
+
+    def __str__(self):
+        return self.Name
