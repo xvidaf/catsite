d6681cc36543dfefd1ed617115c7a5941df000c1
Liqkerin <vida.filip@gmail.com>
2022-01-30 17:17:55 +0100

    Implemented a foundation for the search function. Added a new option to the header for going to the advanced search functionality. Added means to visually move between the paginated cats, added an option to go to a specific page.

--- catapp/urls.py
+++ catapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('cat/<cat_id>', views.cat, name="cat"),
     path('cats', views.cat_list, name="cats"),
+    path('search_cat', views.search_cat, name="search_cat"),
+    path('advanced_search', views.advanced_search, name="advanced_search"),
 ]
--- catapp/views.py
+++ catapp/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cat
 # Create your views here.
 
@@ -13,21 +13,39 @@ def cat(request, cat_id):
         single_cat = Cat.objects.get(pk=cat_id)
         return render(request, 'cat.html', {'single_cat': single_cat})
     except Cat.DoesNotExist:
-        raise Http404("Poll does not exist")
+        raise Http404("Cat does not exist")
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def cat_list(request):
     cat_all = Cat.objects.all()
-    page = request.GET.get('page', 1)
-
+    if request.method == "GET":
+        page = request.GET.get('page', 1)
+    if request.method == "POST":
+        page = request.POST.get('page_search')
+        if not page:
+            return redirect(request.META.get('HTTP_REFERER'))
     paginator = Paginator(cat_all, 50)
     try:
         cats = paginator.page(page)
     except PageNotAnInteger:
-        cats = paginator.page(1)
+        #cats = paginator.page(1)
+        return redirect(request.META.get('HTTP_REFERER'))
     except EmptyPage:
         cats = paginator.page(paginator.num_pages)
 
     return render(request, 'cat_list.html', { 'cats': cats })
 
+def search_cat(request):
+    if request.method == "POST":
+        pageSearch = request.POST.get('cat_search')
+        if pageSearch:
+            print(pageSearch)
+            return render(request, 'search_cat.html', {'pageSearch' : pageSearch})
+        else:
+            return redirect(request.META.get('HTTP_REFERER'))
+    else:
+        return redirect(request.META.get('HTTP_REFERER'))
+
+def advanced_search(request):
+    return render(request, 'advanced_search.html', {})
