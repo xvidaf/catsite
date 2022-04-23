8970ac73d41bbfb1ff116811e577e65ca8bb6b69
Liqkerin <vida.filip@gmail.com>
2022-04-23 12:48:35 +0200

    Changed database credentials to os.environ

--- cat_site/settings.py
+++ cat_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,15 +84,26 @@ WSGI_APPLICATION = 'cat_site.wsgi.application'
 #     }
 # }
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'catDatabase',
+       'NAME': 'catDatabase',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
+       'HOST': 'localhost',
+       'PORT': '5433',
+   }
+}'''
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS']
+        'PORT': '5432 ',
+  }
 }
 
 
