4709ab6f399e2adddcb09d3b61e14f3e230fd971
Liqkerin <vida.filip@gmail.com>
2022-04-23 14:38:32 +0200

    Fix

--- cat_site/settings.py
+++ cat_site/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'NAME': os.environ['DBNAME'],
         'HOST': os.environ['DBHOST'],
         'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS']
+        'PASSWORD': os.environ['DBPASS'],
         'PORT': '5432 ',
   }
 }
