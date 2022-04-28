3944ae76f1971529a484ef2a7cfadd765e6bc649
Liqkerin <vida.filip@gmail.com>
2022-04-28 21:22:50 +0200

    Added static files

--- cat_site/settings.py
+++ cat_site/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -105,7 +107,6 @@ DATABASES = {
         'PORT': '5432 ',
   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
